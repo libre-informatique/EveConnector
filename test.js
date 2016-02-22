39ca4a87f84ce2fe08bc5e1d8badbcb94013a030
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-02-22 16:08:54 +0100

    minor

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
+var fs = require('fs');
+var https_options = {
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert: fs.readFileSync(__dirname + "/server.crt")
+}
+
 var EC = require('./eve-connector-server.js').createServer(8164);
