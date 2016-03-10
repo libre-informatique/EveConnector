a105efc15cb11f5fcfe808d3190aa660875ae8c4
Marcos BDM <marcos.bezerra@libre-informatique.fr>
2016-03-10 15:43:38 +0100

    Removing EPT emulator from npm

--- start-ept.js
+++ start-ept.js
@@ -8,7 +8,12 @@ var https_options = {
 var EC = require('./eve-connector-server.js')
 var server = new EC.Server(8164, https_options);
 
-// var express = require('express');
+
+/**
+ * EPT emulator test page
+ */
+var express = require('express');
 server.express.get('/ept-client', function (req, res) {
   res.sendFile(__dirname + '/web/ept-client.html');
 });
+server.express.use('/ept-emulator', express.static(__dirname + '/ept-emulator'));
